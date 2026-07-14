d53a15c042d5f10fe3b74f3ec5424acbce700cbf
dimas pradipto <dpradipto8@gmail.com>
2026-07-14 09:47:44 +0700

    feat: initialize authentication scaffolding and integrate yajra/laravel-datatables package

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/', 'index')->name('login');
+    Route::post('login', 'login');
+    Route::post('logout', 'logout')->name('logout');
 });
-
-
